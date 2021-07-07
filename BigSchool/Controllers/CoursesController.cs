a348d8933b7288116e4fb2550854678b57d750ef
HoangSonHT <hoangcongson23031999@gmail.com>
2021-07-07 17:50:52 +0700

    Initial commit

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,53 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        BigSchoolContext context = new BigSchoolContext();
+        Course objCourse = new Course();
+        // GET: Courses
+
+
+        public ActionResult Create()
+        {
+            objCourse.ListCategory = context.Categories.ToList();
+
+            return View(objCourse);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course objCourse)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            //khong xet vailid LectureId vi bang user dang nhap
+            ModelState.Remove("LecturerId");
+            if (!ModelState.IsValid)
+            {
+                objCourse.ListCategory = context.Categories.ToList();
+                return View("Create", objCourse);
+            }
+
+            //lay login user ID
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LecturerId = user.Id;
+            //add vao csdl
+            context.Courses.Add(objCourse);
+            context.SaveChanges();
+
+            //tro ve home , Action Index
+            return RedirectToAction("Index", "Home");
+        }
+    }
+
+
+}
---
+++ BigSchool/Models/Course.cs
@@ -0,0 +1,36 @@
+﻿namespace BigSchool.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Course")]
+    public partial class Course
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string LecturerId { get; set; }
+
+        [Required(ErrorMessage = "Địa điểm không được để trống")]
+        [StringLength(255)]
+        public string Place { get; set; }
+
+        //[Required(ErrorMessage = "Chưa nhập ngày tháng năm")]
+        //[Range(typeof(DateTime), "7/1/2021", "7/31/2021", ErrorMessage = "Giá trị của {0} phải từ ngày {1} đến ngày {2}")]
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        //add List category
+        public List<Category> ListCategory = new List<Category>();
+
+        public string Name;
+    }
+}
